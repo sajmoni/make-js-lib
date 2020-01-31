c85741e564d766ad99f50bb7f41602d7f3318c70
Simon Lagos <simon.lagos.sallhed@gmail.com>
2020-01-31 14:54:24 +0900

    First working version of the tool

---
+++ README.md
@@ -0,0 +1,59 @@
+<h1 align="center">
+  make-js-lib
+</h1>
+<h4 align="center">
+  A command line tool to generate javascript libraries
+</h4>
+
+<div align="center">
+  <img src="https://badgen.net/npm/v/make-js-lib?icon=npm" />
+  <img src="https://badgen.net/npm/dw/make-js-lib?icon=npm" />
+</div>
+<div align="center">
+  <img src="https://badgen.net/github/last-commit/sajmoni/make-js-lib?icon=github" />
+</div>
+
+---
+
+## :sparkles: Features
+
+ - :sparkles: [`parcel`](https://github.com/parcel-bundler/parcel) - Very fast, zero config, module bundler
+
+ - :policeman: [`eslint`](https://github.com/eslint/eslint) and [`typescript`](https://github.com/microsoft/TypeScript) - Ensure code quality
+
+ - :arrow_up: [`np`](https://github.com/sindresorhus/np) - A better `npm publish`
+
+ - :straight_ruler: [`ava`](https://github.com/avajs/ava) - Super simple test framework
+
+ <!-- - :book: [`docosaurus`]() -  -->
+
+ <!-- - `semantic versioning workflow` -  -->
+
+ <!-- - [`pre-commit`]() + [`husky`]() - Ensure code quality on each commit -->
+
+ <!-- - Workflow to test your library locally before publishing -->
+
+ <!-- - Readme badges -->
+
+ <!-- - Example app on Netlify? -->
+
+<!-- For command line tools: -->
+
+ <!-- - [`commander`]()  -->
+
+<!-- parcel-plugin-shebang -->
+
+## Usage
+
+`npx make-js-lib [library name]`
+
+_Usage with `npx` ensures that you are always using the latest version_
+
+<!-- ## :computer: Develop
+
+### Commands
+
+Command | Description
+------- | -----------
+`yarn build` | Generate files in the `dist` folder
+`yarn release` | Start the process to release a new version -->
---
+++ index.js
@@ -0,0 +1,164 @@
+#!/usr/bin/env node
+
+const fs = require('fs-extra')
+const commander = require('commander')
+const chalk = require('chalk')
+const path = require('path')
+const os = require('os')
+
+const packageJson = require('./package.json')
+const spawnCommand = require('./spawnCommand.js')
+
+// The structure of this code is inspired by the create-react-app source code
+
+const displayDoneMessage = ({ name, rootPath }) => {
+  console.log()
+  console.log(`Success! Created ${chalk.cyan(name)} at ${chalk.cyan(rootPath)}`)
+  console.log()
+  console.log('Start by typing:')
+  console.log()
+  console.log(chalk.cyan(`  cd ${name}`))
+  console.log()
+  console.log(`  ${chalk.cyan('yarn start')}`)
+  console.log()
+  console.log('Good luck!')
+  console.log()
+}
+
+let projectName
+
+const program = new commander.Command(packageJson.name)
+  .version(packageJson.version)
+  .arguments('<project-directory>')
+  .usage(`${chalk.green('<project-directory>')} [options]`)
+  .action((name) => {
+    projectName = name
+  })
+  .on('--help', () => {
+    console.log()
+    console.log(`    Only ${chalk.green('<project-directory>')} is required.`)
+    console.log()
+  })
+  .parse(process.argv)
+
+
+if (typeof projectName === 'undefined') {
+  console.log()
+  console.error('Please specify the project directory:')
+  console.log(
+    `  ${chalk.cyan(program.name())} ${chalk.green('<project-directory>')}`,
+  )
+  console.log()
+  console.log('For example:')
+  console.log(`  ${chalk.cyan(program.name())} ${chalk.green('my-js-lib')}`)
+  console.log()
+  console.log(
+    `Run ${chalk.cyan(`${program.name()} --help`)} to see all options.`,
+  )
+  console.log()
+  process.exit(1)
+}
+
+
+const rootPath = path.resolve(projectName)
+const appName = path.basename(rootPath)
+
+if (fs.existsSync(rootPath)) {
+  console.log()
+  console.log(`${chalk.red('Project folder already exists')} ${chalk.green(rootPath)}`)
+  console.log()
+  process.exit(1)
+}
+
+console.log()
+console.log(`Creating a new project in ${chalk.green(rootPath)}`)
+console.log()
+
+fs.mkdirSync(rootPath)
+
+const packageJsonTemplate = {
+  name: appName,
+  version: '0.0.1',
+  scripts: {
+    start: 'parcel src/index.js --no-autoinstall --no-cache',
+    build: 'rm -rf dist && parcel build src/index.js --no-cache',
+    test: 'ava',
+    lint: 'eslint .',
+    typecheck: 'tsc --module commonjs --allowJs --checkJs --noEmit --target es2016 src/*.js',
+    'check-all': 'yarn lint && yarn typecheck ',
+    release: 'yarn build && np',
+    clean: `rm -f ${appName}.tgz`,
+    'build-test': `yarn clean && yarn build && yarn pack --filename ${appName}.tgz && cd example && yarn refresh`,
+  },
+  ava: {
+    require: [
+      '@babel/register',
+    ],
+  },
+}
+
+fs.writeFileSync(
+  path.join(rootPath, 'package.json'),
+  JSON.stringify(packageJsonTemplate, null, 2) + os.EOL,
+)
+
+// TODO: Remove if no dependencies are needed
+const allDependencies = [
+  'lodash',
+]
+
+// TODO: Versioning? Tag releases, generate patch notes
+const devDependencies = [
+  // * Code quality
+  'eslint',
+  'eslint-config-airbnb-base',
+  'eslint-plugin-import',
+  'typescript',
+  // * --
+  // * Testing
+  '@babel/preset-env',
+  '@babel/register',
+  'ava',
+  // * --
+  // * Other
+  'parcel-bundler',
+  'np',
+  // * --
+]
+
+console.log('Installing packages. This might take a couple of minutes.')
+console.log()
+
+const pathArg = ['--cwd', rootPath]
+const defaultArgs = ['add', '--exact']
+
+const command = 'yarnpkg'
+const productionArgs = defaultArgs.concat(allDependencies).concat(pathArg)
+const devArgs = defaultArgs.concat('--dev').concat(devDependencies).concat(pathArg)
+
+spawnCommand({ command, args: productionArgs })
+  .then(() => spawnCommand({ command, args: devArgs }))
+  .then(() => {
+    console.log()
+    console.log('Copying files from template')
+
+    const templateDirectory = `${__dirname}/template`
+    console.log('TCL: rootPath', rootPath)
+    console.log('TCL: templateDirectory', templateDirectory)
+
+    try {
+      fs.copySync(templateDirectory, rootPath)
+    } catch (error) {
+      console.log('ERROR3', error)
+    }
+
+    // TODO: Create an initial commit
+    displayDoneMessage({ name: projectName, rootPath })
+  }).catch((reason) => {
+    console.log()
+    console.log(chalk.red('Aborting installation.'))
+    console.log(`Command failed: ${chalk.cyan(reason.command)}`)
+    console.log()
+  })
+
+// TODO: Tests
